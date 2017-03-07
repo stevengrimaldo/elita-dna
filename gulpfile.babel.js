cb0b517ac98de2a867a120a5beee43ab1bb7bf9b
Steven Grimaldo <srgrimaldo@me.com>
2017-03-06 17:07:17 -0800

    first commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,198 @@
+import gulp from 'gulp';
+import autoprefixer from 'autoprefixer';
+import browserify from 'browserify';
+import watchify from 'watchify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import eslint from 'gulp-eslint';
+import babelify from 'babelify';
+import uglify from 'gulp-uglify';
+import rimraf from 'rimraf';
+import notify from 'gulp-notify';
+import browserSync, { reload } from 'browser-sync';
+import sourcemaps from 'gulp-sourcemaps';
+import postcss from 'gulp-postcss';
+import rename from 'gulp-rename';
+import nested from 'postcss-nested';
+import inline from 'postcss-strip-inline-comments';
+import cssnano from 'gulp-cssnano';
+import imagemin from 'gulp-imagemin';
+import pngquant from 'imagemin-pngquant';
+import runSequence from 'run-sequence';
+import eStream from 'event-stream';
+import factor from 'factor-bundle';
+import assemble from 'assemble';
+import scss from 'postcss-scss';
+import handlebarsHelpers from 'handlebars-helpers';
+import prettify from 'gulp-prettify';
+import sass from 'gulp-sass';
+import csswring from 'csswring';
+
+const app = assemble();
+
+const paths = {
+  bundle: 'app.js',
+  entry: './src/global/app.js',
+  srcCss: './src/scss/**/**/*.scss',
+  srcImg: './src/media/img/**/*',
+  srcLint: ['./src/js/polymap.js'],
+  dist: './dist',
+  distJs: './dist/js',
+  distImg: './dist/media/img',
+  distDeploy: './dist/**/*'
+};
+
+const entries = [
+  './src/global/app.js',
+  './src/components/accordion/accordion.js',
+  './src/components/articles/articles.js',
+  './src/components/documents/documents.js',
+  './src/components/featured-content/featured-content.js',
+  './src/components/hero/hero.js',
+  './src/components/locations/locations.js',
+  './src/components/profile/profile.js',
+  './src/components/resources/resources.js',
+  './src/components/reviews/reviews.js',
+  './src/components/solutions/solutions.js',
+  './src/components/split-content/split-content.js',
+  './src/components/team/team.js',
+  './src/components/tools/tools.js'
+];
+
+const output = [
+  './dist/js/app.js',
+  './dist/js/components/accordion.js',
+  './dist/js/components/articles.js',
+  './dist/js/components/documents.js',
+  './dist/js/components/featured-content.js',
+  './dist/js/components/hero.js',
+  './dist/js/components/locations.js',
+  './dist/js/components/profile.js',
+  './dist/js/components/resources.js',
+  './dist/js/components/reviews.js',
+  './dist/js/components/solutions.js',
+  './dist/js/components/split-content.js',
+  './dist/js/components/team.js',
+  './dist/js/components/tools.js'
+];
+
+const customOpts = {
+  entries: entries,
+  debug: true,
+  cache: {},
+  packageCache: {}
+};
+
+const options = {
+  locale: 'en-GB',
+  timestamp: Date.now(),
+  assets: './dist/media/img/'
+}
+
+const opts = Object.assign({}, watchify.args, customOpts);
+
+gulp.task('clean', cb => {
+  rimraf('./dist/js/components/*', cb);
+});
+
+gulp.task('browserSync', () => {
+  browserSync({
+    server: {baseDir: './dist/'}
+  });
+});
+
+gulp.task('watchify', () => {
+  const bundler = watchify(browserify(opts));
+
+  function rebundle() {
+    return bundler.plugin(factor, {o: output})
+      .bundle()
+      .on('error', notify.onError())
+      .pipe(source('common.js'))
+      .pipe(gulp.dest(paths.distJs))
+      .pipe(reload({stream: true}));
+  }
+
+  bundler.transform(babelify)
+  .on('update', rebundle);
+  return rebundle();
+});
+
+gulp.task('browserify', () => {
+  return browserify({entries: entries})
+  .plugin(factor, {o: output})
+  .bundle()
+  .pipe(source('common.js'))
+  .pipe(buffer())
+  .pipe(uglify())
+  .pipe(gulp.dest(paths.distJs));
+});
+
+gulp.task('styles', () => {
+  gulp.src('./src/**/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(postcss([autoprefixer({
+      browsers: ['> 2%'],
+      cascade: false,
+      map: true,
+      remove: true
+    }), csswring({removeAllComments: true})]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('images', () => {
+  gulp.src(paths.srcImg)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(paths.distImg));
+});
+
+gulp.task('html', () => {
+  // Find layouts and partials
+  app.layouts('./src/global/default.hbs');
+  app.partials('./src/components/**/*.hbs');
+
+  // Add data
+  app.data('./src/data/*.{json,yml}');
+  app.data({imagePath: './dist/media/img/'});
+
+  // Add classic helpers
+  app.helpers(handlebarsHelpers(), app.helpers);
+
+  // Build templates
+  return app.src('./src/pages/*.hbs')
+    .pipe(app.renderFile(options))
+    .pipe(rename({extname: '.html'}))
+    .pipe(prettify({indent_size: 2}))
+    .pipe(app.dest('./dist/'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('lint', () => {
+  gulp.src(paths.srcLint)
+  .pipe(eslint())
+  .pipe(eslint.format());
+});
+
+gulp.task('watchTask', () => {
+  gulp.watch(paths.srcImg, ['images']);
+  gulp.watch(paths.srcCss, ['styles']);
+  gulp.watch(paths.srcLint, ['lint']);
+  gulp.watch('./src/pages/*.hbs', ['html']);
+});
+
+gulp.task('watch', cb => {
+  runSequence('clean', ['browserSync', 'watchTask', 'watchify', 'styles', 'lint', 'html', 'images'], cb);
+});
+
+gulp.task('build', cb => {
+  process.env.NODE_ENV = 'production';
+  runSequence('clean', ['browserify', 'styles', 'html', 'images'], cb);
+});
---
+++ src/global/app.js
@@ -0,0 +1,19 @@
+const components = [
+  'hero',
+  'featured-content',
+  'resources'
+];
+const componentLength = components.length;
+
+function loadCss(name) {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = '/css/components/' + name + '/' + name + '.min.css';
+  document.getElementsByTagName('head')[0].appendChild(link);
+}
+
+for (let i = 0; i < componentLength; i++) {
+  if (typeof(document.querySelector('.' + components[i])) !== 'undefined' && document.querySelector('.' + components[i]) !== null) {
+    loadCss(components[i]);
+  }
+}
