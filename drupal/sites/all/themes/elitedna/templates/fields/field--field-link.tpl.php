c86885bca642fd22a0dc548c80b12096e2f5c21b
Steven Grimaldo <srgrimaldo@me.com>
2017-05-24 23:01:00 -0500

    updating mobile and field

--- drupal/sites/all/themes/elitedna/templates/fields/field--field-link.tpl.php
+++ drupal/sites/all/themes/elitedna/templates/fields/field--field-link.tpl.php
@@ -1,3 +1,6 @@
 <?php foreach ($items as $delta => $item): ?>
-  <a href="<?php print render($item['#element']['display_url']); ?>" rel="<?php print render($item['#element']['attributes']['rel']); ?>" class="cta cta--<?php print $item['#element']['attributes']['class']; ?>"><?php print render($item['#element']['title']); ?></a>
+  <a href="<?php print render($item['#element']['display_url']); ?>"
+    rel="<?php print render($item['#element']['attributes']['rel']); ?>"
+    target="<?php print render($item['#element']['attributes']['target']); ?>"
+    class="cta cta--<?php print $item['#element']['attributes']['class']; ?>"><?php print render($item['#element']['title']); ?></a>
 <?php endforeach; ?>
