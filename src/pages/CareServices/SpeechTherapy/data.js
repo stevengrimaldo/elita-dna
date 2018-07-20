17b2a03a57c6bacad2819e0b6522cdbb74df3a40
Steven Grimaldo <sgrimaldo@gsandf.com>
2018-07-20 15:15:50 -0500

    updating pdfs and adding remaining bios

--- src/pages/CareServices/SpeechTherapy/data.js
+++ src/pages/CareServices/SpeechTherapy/data.js
@@ -5,87 +5,81 @@ export const data = {
     questions: [
       {
         text: `
+          <p>Spoken language provides the foundation for the development of reading and writing; Since spoken and written language have a reciprocal relationship, each builds on the other to result in general language and literacy competence, starting early and continuing through childhood into adulthood. Instruction in spoken language can result in growth in written language, and instruction in written language can result in growth in spoken language.</p>
+        `,
+        title: 'Literacy',
+      },
+      {
+        text: `
+          <p>These are the social communication skills or the “rules” we follow when we talk to others. These would include knowing how to use language for different purpose (greeting, informing, demanding, promising), changing language for the listener (we don’t speak to adults and babies the same way) and following rules for conversation (stay on topic, take turns when talking, etc).</p>
+        `,
+        title: 'Pragmatics',
+      },
+      {
+        text: `
+          <p>This helps young childrens speech, language and hearing disorders. It can prevent or reduce problems with communication, reading, learning and social skills. Early identification will help school readiness and build communication skills at home, school and in the  community.</p>
+        `,
+        title: 'Early Intervention',
+      },
+      {
+        text: `
+          <p>These include a wide spectrum of disorders that may interfere with a childs memory, attention, executive functioning, and/or emotion.</p>
+        `,
+        title: 'Cognitive Communication Disorders',
+      },
+      {
+        text: `
           <p>A Communication disorder or delay is impairment in one’s ability to process, send, receive, or comprehend written or spoken language. Communication disorders can range from mild to severe and can be developmental or acquired. With children, studies show that the earlier your child gets help, the greater their progress will be.</p>
         `,
         title: 'Communication Disorders & Delays',
       },
       {
         text: `
-          <p></p>
+          <p>Language disorders or language impairments are disorders that involve the processing of linguistic information. Problems that may be experienced can involve grammar (syntax and/or morphology), semantics (meaning), or other aspects of language.</p>
         `,
-        title: 'Language Disorders?',
+        title: 'Language Disorders',
       },
       {
         text: `
-          <p></p>
-        `,
-        title: 'Central Auditory Processing Disorders (CAPD)',
-      },
-      {
-        text: `
-          <p></p>
+          <p>A speech sound disorder (SSD) is a speech disorder in which some speech sounds (called phonemes) in a child's (or, sometimes, an adult's) language are either not produced, not produced correctly, or are not used correctly.</p>
         `,
         title: 'Speech Sound Disorders',
       },
       {
         text: `
-          <p></p>
+          <p>Childhood apraxia of speech (CAS) has a number of possible causes, but in many cases a cause can't be determined. Apraxia is a motor speech disorder that makes it hard for children to speak. It can take a lot of work to learn to say sounds and words better.</p>
         `,
         title: 'Childhood Apraxia of Speech',
       },
       {
         text: `
-          <p></p>
+          <p>A fluency disorder, which is often referred to as “stuttering”, is characterized by primary (core) and secondary behaviors. Primary behaviors may include repetitions of sounds, syllables, or whole words; prolongations of single sounds; or blocks of airflow or voicing during speech. Secondary behaviors develop over time as learned reactions to the core behaviors and are categorized as avoidance behaviors. They may include hesitations, interjections of sounds, syllables, or words; word revisions or complete changes in words; or motor movements associated with stuttering (such as eye blinking, loss of eye contact, extraneous movements, to name a few).</p>
         `,
         title: 'Stuttering & Fluency Disorders',
       },
       {
         text: `
-          <p></p>
-        `,
-        title: 'Traumatic Brain Injury',
-      },
-      {
-        text: `
-          <p></p>
-        `,
-        title: 'Voice Disorders',
-      },
-      {
-        text: `
-          <p></p>
+          <p>A feeding disorder, in infancy or early childhood, is a child's refusal to eat certain food groups, textures, solids or liquids for a period of at least one month, which causes the child to not gain enough weight, grow naturally or cause any developmental delays. A swallowing disorder is also called dysphagia (dis-FAY-juh). Swallowing happens in three stages, or phases. You can have a problem in one or more of these phases.</p>
         `,
         title: 'Feeding & Swallowing',
       },
       {
         text: `
-          <p></p>
+          <p>Speech-language therapy addresses a range of challenges often faced by persons with autism. For instance, some individuals on the autism spectrum do not speak, while others love to talk but have difficulty using conversational speech and/or understanding the nuances of language and nonverbal cues when talking with others. Speech therapy is designed to coordinate the mechanics of speech with the meaning and social use of language.</p>
         `,
         title: 'Autism',
       },
       {
         text: `
-          <p></p>
+          <p>Augmentative and alternative communication (AAC) includes use of symbols, aids, strategies, and techniques to enhance the communication process. Sign language, various communication boards, and both manual and electronic devices are all assistive technology tools that assist patients in communication. Our speech therapists can help your child in the use of these devices in order to maximize their benefit.</p>
         `,
         title: 'Assistive Technology',
       },
       {
         text: `
-          <p></p>
+          <p>Include difficulty with reading, writing or spelling.</p>
         `,
-        title: 'Acquired Apraxia of Speech',
-      },
-      {
-        text: `
-          <p></p>
-        `,
-        title: 'Dysarthia',
-      },
-      {
-        text: `
-          <p></p>
-        `,
-        title: 'Aphasia',
+        title: 'Academics',
       },
     ],
     title: 'Conditions We Specialize in:',
