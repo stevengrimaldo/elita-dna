34efcd42bcf052e284cb14fbf13e88b9703ac3c8
Steven Grimaldo <sgrimaldo@gsandf.com>
2018-08-23 11:03:38 -0500

    teletherapy changes

--- src/pages/CareServices/Teletherapy/data.js
+++ src/pages/CareServices/Teletherapy/data.js
@@ -4,7 +4,7 @@ export const data = {
       links: [],
       subTitle: '',
       text: `
-        <p>This is an app based therapy platform, connecting qualified mental health professionals and adults.<br />Self-pay & most insurances accepted excluding: (BCBS and Medicare), email us at <a href="mailto:teletherapy@elitednatherapy.com">teletherapy@elitednatherapy.com</a> for more info.</p>
+        <p>This is an app based therapy platform, connecting qualified mental health professionals and ages 13+.<br />Self-pay & most insurances accepted excluding: (BCBS and Medicare), email us at <a href="mailto:teletherapy@elitednatherapy.com">teletherapy@elitednatherapy.com</a> for more info.</p>
       `,
       title: 'Teletherapy',
     },
@@ -40,7 +40,7 @@ export const data = {
   whatIsTeletherapy: {
     links: [],
     text: `
-      <p>Elite DNA Connect, an app based therapy platform, connecting qualified mental health professionals and adults dealing with mental health issues such as depression, poor stress management and anxiety, among others. Receive therapy on-the-go during business hours (8am-6pm Monday-Friday) from your tablet or smart-phone. Self-pay & most insurances accepted excluding: (BCBS and Medicare)</p>
+      <p>Elite DNA Connect, an app based therapy platform, connecting qualified mental health professionals and adults dealing with mental health issues such as depression, poor stress management and anxiety, among others. Receive therapy on-the-go during business hours (8am-6pm Monday-Friday) from your tablet or smart-phone. Self-pay & most insurances accepted excluding: BCBS, Medicare, Aetna and some Cignas.</p>
     `,
     title: 'We are proud to introduce Teletherapy',
   },
